67d6ca6b29e49c324085fc1f0036dc90d2ee1d44
balta2205 <baltasar.oostdijk@gmail.com>
2025-05-29 14:22:03 -0300

    Correccion ej2

--- Ejercicio_2/headers/ProcesadorFigura.h
+++ Ejercicio_2/headers/ProcesadorFigura.h
@@ -40,19 +40,19 @@ class Procesador {
     public:
         void Area(T& figura) { // Metodo para calcular el area de la figura.
             if constexpr (std::is_same_v<T, Circulo>){
-                std::cout << "Area del circulo: " << circulo.getRadio() * circulo.getRadio() * PI << std::endl;
+                std::cout << "Area del circulo: " << figura.getRadio() * figura.getRadio() * PI << std::endl;
             }
             else if constexpr (std::is_same_v<T, Elipse>){
-                std::cout << "Area de la elipse: " << elipse.getSemiMayor() * elipse.getSemiMenor() * PI << std::endl;
+                std::cout << "Area de la elipse: " << figura.getSemiMayor() * figura.getSemiMenor() * PI << std::endl;
             }
             else if constexpr (std::is_same_v<T, Rectangulo>){
-                std::cout << "Area del rectangulo: " << rectangulo.getAncho() * rectangulo.getLargo() << std::endl;
+                std::cout << "Area del rectangulo: " << figura.getAncho() * figura.getLargo() << std::endl;
             }
             else {
                 std::cout << "ERROR: No es posible calcular area." << std::endl;
             }
         }
-}
+};
 
 
 #endif // PROCESADORFIGURA.H
